c071199faa34f7ee1d38a3c327d347b62fde06fd
Arman <armandwi.pangestu7@gmail.com>
2024-06-03 00:49:23 +0700

    feat(db): add works and educations seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,16 @@
 import prisma from "../db/prisma.js";
+import { seedEducations } from "./seed/educations.seed.js";
 import { cleanDatabase } from "./seed/helper/clean.seedHelper.js";
 import { seedRoles } from "./seed/roles.seed.js";
 import { seedUsers } from "./seed/users.seed.js";
+import { seedWorks } from "./seed/works.seed.js";
 
 async function main() {
     await cleanDatabase();
     await seedRoles();
     await seedUsers(10);
+    await seedWorks();
+    await seedEducations();
 }
 
 main()
