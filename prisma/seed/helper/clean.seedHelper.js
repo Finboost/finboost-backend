85c6470cd0e4090402f0acaa38bcd94d7b8f3600
Arman <armandwi.pangestu7@gmail.com>
2024-06-13 12:34:59 +0700

    feat(db): update seeder

--- prisma/seed/helper/clean.seedHelper.js
+++ prisma/seed/helper/clean.seedHelper.js
@@ -2,9 +2,14 @@ import prisma from "../../../db/prisma.js";
 
 const cleanDatabase = async () => {
     console.log(`\nStart cleaning database...`);
+    console.log(`\nDeleting table users`);
     await prisma.user.deleteMany();
-    await prisma.role.deleteMany();
     console.log(`\nDeleting table roles`);
+    await prisma.role.deleteMany();
+    console.log(`\nDeleting table works`);
+    await prisma.work.deleteMany();
+    console.log(`\nDeleting table educations`);
+    await prisma.education.deleteMany();
 };
 
 export { cleanDatabase };
